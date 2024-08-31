7acdf01b958060e22a9347a1a3956c46a68d0e7b
gui23g <guilherme.carvalho@sou.inteli.edu.br>
2024-08-31 10:59:18 -0300

    fix: Corrige o erro

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,12 +2,10 @@ import numpy as np
 
 def calculate(list):
     if len(list) != 9:
-        raise ValueError("List must contain nine numbers.")
+        raise ValueError("São necessários 9 números")
     
-    # Convert the list into a 3x3 Numpy array
     matrix = np.array(list).reshape(3, 3)
     
-    # Calculate the required statistics
     calculations = {
         'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().tolist()],
         'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().tolist()],
